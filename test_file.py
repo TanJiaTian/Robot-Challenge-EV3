642b743ebfb2fad2f7471875a997a509d83fd73c
MichaelSchwenkschuster <73822057+MichaelSchwenkschuster@users.noreply.github.com>
2020-11-11 10:26:31 +0100

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -3,6 +3,8 @@ import ev3dev.ev3 as ev3
 from time import sleep
 # let one wheel of the Marsrover move foreward
 
+#hallo wie gehts
+
 # Connect the output A to the motor
 motor_left = ev3.LargeMotor('outA')
 # move motor for 1000ms with 500dec/sec
